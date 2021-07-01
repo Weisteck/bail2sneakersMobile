c3fd48752889da9507c6e60044554b1f7c9b6227
weisteck <hugo.celdran@gmail.com>
2021-07-01 12:50:58 +0200

    fetchApi et reinjection dans une flatlist OK

--- src/container/screens/home/index.js
+++ src/container/screens/home/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
-import {View, Text, Button, ImageBackground} from 'react-native';
+import {View, Text, Button, Image} from 'react-native';
 import styles from './style';
 import {Icon} from "react-native-elements";
 
-const image = {uri: ''}
-
-const HomeScreen = ({ navigation }) => {
+const HomeScreen = ({navigation}) => {
   return (
     <View style={styles.main}>
-      <ImageBackground source={} >
-        <Text>Bienvenue sur la page d'accueil de B2S 👟</Text>
-        <Button
-          title='Aller au Details'
-          onPress={()=> navigation.navigate('Details')}
-        />
-        <Button
-          title='Open Drawer'
-          onPress={() => navigation.toggleDrawer()}
-        />
-        <Icon name='home' />
-      </ImageBackground>
+      <Image
+        source={{
+          uri: 'https://github.com/Weisteck/bail2sneakersMobile/blob/works/fetchApiToDataStore/src/image/logoB2S.png?raw=true'
+        }}
+        style={styles.logo}
+      />
+      <Text>Bienvenue sur la page d'accueil de B2S 👟</Text>
+      <Button
+        title='Aller au Details'
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title='Open Drawer'
+        onPress={() => navigation.toggleDrawer()}
+      />
+      <Icon name='home'/>
     </View>
   );
 }
